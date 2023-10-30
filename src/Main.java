8204dacfaf22df3d326e31ec430441ca01af3444
cotucker <klimilya005@gmail.com>
2023-10-30 15:41:33 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Integer a = 1;
+        int b = 1;
+        b = a.intValue();
+        Transport ferrari = new Transport();
+        ferrari.MaxSpead = 300;
+        String info = "Info: " + ferrari.MaxSpead;
+        System.out.println(info);
+    }
+}
+
+ 
---
+++ src/Transport.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class Transport {
+    public double MaxSpead, mass;
+    public String color;
+    public byte[] location;
+
+}
